fb4d5f9d87fd7197ab52f4b827036a6019cdae0f
FVS <feliven@gmail.com>
2026-02-10 09:08:19 -0300

    criado componente de login

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,9 +1,29 @@
 import { Component } from '@angular/core';
 import { BannerComponent } from 'src/app/shared/banner/banner.component';
+import {
+  MatCard,
+  MatCardHeader,
+  MatCardTitle,
+  MatCardContent,
+  MatCardActions,
+} from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-login',
-  imports: [BannerComponent],
+  imports: [
+    BannerComponent,
+    MatCard,
+    MatCardHeader,
+    MatCardTitle,
+    MatCardContent,
+    MatFormFieldModule,
+    MatCardActions,
+    MatInputModule,
+    MatButtonModule,
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
 })
