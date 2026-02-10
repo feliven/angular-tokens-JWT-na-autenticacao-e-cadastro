a60ff47d927768a814a93d221b894f75a590558c
FVS <feliven@gmail.com>
2026-02-10 09:38:35 -0300

    torna página de login um reactive form

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,30 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { BannerComponent } from 'src/app/shared/banner/banner.component';
-import {
-  MatCard,
-  MatCardHeader,
-  MatCardTitle,
-  MatCardContent,
-  MatCardActions,
-} from '@angular/material/card';
+import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
+  templateUrl: './login.component.html',
+  standalone: true,
   imports: [
     BannerComponent,
-    MatCard,
-    MatCardHeader,
-    MatCardTitle,
-    MatCardContent,
+    ReactiveFormsModule,
+    MatCardModule,
     MatFormFieldModule,
-    MatCardActions,
     MatInputModule,
     MatButtonModule,
   ],
-  templateUrl: './login.component.html',
-  styleUrl: './login.component.scss',
+  styleUrls: ['./login.component.scss'],
 })
-export class LoginComponent {}
+export class LoginComponent implements OnInit {
+  formulario!: FormGroup;
+  private formBuilder = inject(FormBuilder);
+
+  ngOnInit(): void {
+    this.formulario = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      senha: ['', [Validators.required, Validators.minLength(6)]],
+    });
+  }
+}
