542849f9beb8f2f860aa52838753034313c6b24a
FVS <feliven@gmail.com>
2026-02-11 14:05:00 -0300

    criado CadastroComponent

---
+++ src/app/pages/cadastro/cadastro.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { BannerComponent } from 'src/app/shared/banner/banner.component';
+import { FormBaseComponent } from 'src/app/shared/form-base/form-base.component';
+
+@Component({
+  selector: 'app-cadastro',
+  imports: [BannerComponent, FormBaseComponent],
+  templateUrl: './cadastro.component.html',
+  styleUrl: './cadastro.component.scss',
+})
+export class CadastroComponent {}
