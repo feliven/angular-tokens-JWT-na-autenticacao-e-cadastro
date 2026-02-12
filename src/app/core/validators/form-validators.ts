4d4f71c098375597b336506d1dbd69a27b29db65
FVS <feliven@gmail.com>
2026-02-12 14:04:20 -0300

    cria factory de validadores

---
+++ src/app/core/validators/form-validators.ts
@@ -0,0 +1,16 @@
+import { AbstractControl, ValidationErrors, ValidatorFn } from '@angular/forms';
+
+export class FormValidations {
+  static ehIgual(outroCampo: string): ValidatorFn {
+    return (control: AbstractControl): ValidationErrors | null => {
+      const valorCampo = control.value;
+      const valorOutroCampo = control.root.get(outroCampo)?.value;
+
+      if (valorCampo !== valorOutroCampo) {
+        return { ehIgual: false };
+      } else {
+        return null;
+      }
+    };
+  }
+}
