c6b4ae0442f19626fb1edfbbcac53a4e1101f98c
FVS-MBA <feliven@gmail.com>
2026-02-14 14:54:12 -0300

    atualiza header com botões de perfil e logout

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,13 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { AsyncPipe } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { RouterLink } from '@angular/router';
+
+import { UserService } from 'src/app/core/services/user.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
-  imports: [RouterLink, MatButtonModule, MatToolbarModule],
+  imports: [RouterLink, AsyncPipe, MatButtonModule, MatToolbarModule],
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  private userService = inject(UserService);
+
+  usuario$ = this.userService.retornarUsuario();
+
+  sair() {
+    this.userService.logout();
+  }
+}
